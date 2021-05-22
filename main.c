106e27f9014154c2fa704b48b2b6540c48d37c37
pixelomer <37103802+pixelomer@users.noreply.github.com>
2021-05-22 13:28:20 +0300

    Add iPad7,12 support

--- main.c
+++ main.c
@@ -34,6 +34,15 @@ const struct device_model devices[] = {
 		.home_button = 0x20f10022c,
 		.home_button_at_right = 1
 	},
+	{
+		.name = "iPad7,12",
+		.volume_up = 0x20f100168,
+		.volume_down = 0x20f100164,
+		.mute_switch = NULL,
+		.power_button = 0x20f1002cc,
+		.home_button = 0x20f1002d0,
+		.home_button_at_right = 1
+	},
 	{ .name = NULL }
 };
 
